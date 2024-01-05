e66783bc2850c957f6450e98a2808e0e386f1eda
Adam DePaolo <amdepaolo@gmail.com>
2024-01-05 18:08:08 -0500

    passes tests

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,13 @@
 # your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, possibilities):
+        matched = []
+        word_letters = [letter for letter in self.word]
+        for possible in possibilities:
+            possible_letters = [letter for letter in possible]
+            if sorted(word_letters) == sorted(possible_letters):
+                matched.append(possible)
+        return matched
